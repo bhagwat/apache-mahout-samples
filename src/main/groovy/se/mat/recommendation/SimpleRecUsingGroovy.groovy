e264f079ccfc093ac10fc1f16773348bea2dabc8
Bhagwat Kumar <bhagwat@intelligrape.com>
2015-07-28 13:02:30 +0530

    Used logger

--- src/main/groovy/se/mat/recommendation/SimpleRecUsingGroovy.groovy
+++ src/main/groovy/se/mat/recommendation/SimpleRecUsingGroovy.groovy
@@ -1,5 +1,6 @@
 package se.mat.recommendation
 
+import groovy.util.logging.Log
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
@@ -9,20 +10,21 @@ import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood
 import org.apache.mahout.cf.taste.recommender.Recommender
 import org.apache.mahout.cf.taste.similarity.UserSimilarity
 
+@Log
 class SimpleRecUsingGroovy {
     public static void main(String[] args) {
-        //Loading data (ratings) from file
+        log.info "Loading data (ratings) from file"
         DataModel model = new FileDataModel(getFile("data/ua.base"))
-        //User pearson correlation for computing similarity
+        log.info "User pearson correlation for computing similarity"
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model)
-        //Limiting the area of users for comparing to
+        log.info "Limiting the area of users for comparing to"
         UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.5, similarity, model)
-        //Sending everything to recommendation engine
+        log.info "Sending everything to recommendation engine"
         Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity)
-        //Recommending 5 times for first user (ID:1)
-        //Presenting recommendations
+        log.info "Recommending 5 times for first user (ID:1)"
+        log.info "Presenting recommendations"
         recommender.recommend(2, 5).each { recommendedItem ->
-            println(recommendedItem);
+            log.info ">>>>>>>>>>>>>>" + recommendedItem
         }
     }
 
